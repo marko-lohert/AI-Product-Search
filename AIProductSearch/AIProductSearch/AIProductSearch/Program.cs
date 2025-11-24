82b83a71ffeb5878fdb520131e72010332f2dd00
marko-lohert <lohert.marko@gmail.com>
2025-11-24 21:50:23 +0100

    Add MudBlazor (Blazor Component Library) and use it for UI.

--- AIProductSearch/AIProductSearch/AIProductSearch/Program.cs
+++ AIProductSearch/AIProductSearch/AIProductSearch/Program.cs
@@ -1,5 +1,6 @@
 using AIProductSearch.Components;
 using AIProductSearch.DAL;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddMudServices();
 
 builder.Services.AddSingleton(new Products($"Data Source={builder.Configuration["Database:FullFileName"]}"));
 
